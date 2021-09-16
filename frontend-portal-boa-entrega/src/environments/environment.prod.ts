c0959e59224192d716890cbdd4c594a6a1aca71d
PabloChristian <snakepablo@hotmail.com>
2021-09-16 11:24:27 -0300

    atualização valor variavel

--- frontend-portal-boa-entrega/src/environments/environment.prod.ts
+++ frontend-portal-boa-entrega/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'http://awseb-AWSEB-1EGV4QP5DOBP2-1048199792.us-east-2.elb.amazonaws.com/api/',
-  imagensUrl: 'http://awseb-AWSEB-1EGV4QP5DOBP2-1048199792.us-east-2.elb.amazonaws.com/'
+  apiUrlv1: 'http://elb-boacompra-cadastro-1259245550.us-east-2.elb.amazonaws.com/api/',
+  imagensUrl: 'http://elb-boacompra-cadastro-1259245550.us-east-2.elb.amazonaws.com/'
   //apiUrlv1: 'http://boaentregacadastrov2-env.eba-pztifugw.us-east-2.elasticbeanstalk.com/api/',
   //imagensUrl: 'http://boaentregacadastrov2-env.eba-pztifugw.us-east-2.elasticbeanstalk.com/'
 };
