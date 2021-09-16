5f4d1473cefb5e948b09055bfa33176e17681ddf
PabloChristian <snakepablo@hotmail.com>
2021-09-16 14:49:41 -0300

    atualização URL

--- frontend-portal-boa-entrega/src/environments/environment.prod.ts
+++ frontend-portal-boa-entrega/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'http://elb-boacompra-cadastro-1259245550.us-east-2.elb.amazonaws.com/api/',
-  imagensUrl: 'http://elb-boacompra-cadastro-1259245550.us-east-2.elb.amazonaws.com/'
-  //apiUrlv1: 'http://boaentregacadastrov2-env.eba-pztifugw.us-east-2.elasticbeanstalk.com/api/',
-  //imagensUrl: 'http://boaentregacadastrov2-env.eba-pztifugw.us-east-2.elasticbeanstalk.com/'
+  apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
+  imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/'
 };
