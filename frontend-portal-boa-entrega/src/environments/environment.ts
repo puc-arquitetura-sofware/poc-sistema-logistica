56bebb9af394b6bb0eb425bedf4777b9332eceeb
PabloChristian <snakepablo@hotmail.com>
2021-09-19 20:10:37 -0300

    atualização url

--- frontend-portal-boa-entrega/src/environments/environment.ts
+++ frontend-portal-boa-entrega/src/environments/environment.ts
@@ -4,10 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrlv1: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/',
-  imagensUrl: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/'
-  //apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
-  //imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/'
+  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com//api/',
+  imagensUrl: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/'
 };
 
 /*
