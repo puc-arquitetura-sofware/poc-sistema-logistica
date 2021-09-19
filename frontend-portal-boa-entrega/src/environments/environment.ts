805cb4bd1ac1525655f1038a474aacc66f3c124b
PabloChristian <snakepablo@hotmail.com>
2021-09-19 11:17:00 -0300

    inclusão de linha faltando

--- frontend-portal-boa-entrega/src/environments/environment.ts
+++ frontend-portal-boa-entrega/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  production: true,
   apiUrlv1: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/',
   imagensUrl: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/'
   //apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
