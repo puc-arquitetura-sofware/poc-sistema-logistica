d8f5f2016b7d8a0036f4cef20e0f201701f0d0dd
PabloChristian <snakepablo@hotmail.com>
2021-09-19 20:38:22 -0300

    correção URL

--- frontend-portal-boa-entrega/src/environments/environment.prod.ts
+++ frontend-portal-boa-entrega/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com//api/',
+  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/',
   imagensUrl: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/'
 };
--- frontend-portal-boa-entrega/src/environments/environment.ts
+++ frontend-portal-boa-entrega/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com//api/',
+  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/',
   imagensUrl: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/'
 };
 
