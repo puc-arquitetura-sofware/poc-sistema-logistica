58a2d0e4a388b8fd40d5551ef816f3a35ff665b2
PabloChristian <snakepablo@hotmail.com>
2021-09-19 11:12:24 -0300

    atualização URL back

--- frontend-portal-boa-entrega/src/environments/environment.prod.ts
+++ frontend-portal-boa-entrega/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
-  imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/'
+  apiUrlv1: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/',
+  imagensUrl: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/'
+  //apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
+  //imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/'
 };
--- frontend-portal-boa-entrega/src/environments/environment.ts
+++ frontend-portal-boa-entrega/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
-  imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/'
+  apiUrlv1: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/',
+  imagensUrl: 'http://api-boacompra-cadastro.us-east-2.elasticbeanstalk.com/api/'
+  //apiUrlv1: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/',
+  //imagensUrl: 'http://boacompracadastro-env.eba-dbh34kvt.us-east-2.elasticbeanstalk.com/api/'
 };
 
 /*
