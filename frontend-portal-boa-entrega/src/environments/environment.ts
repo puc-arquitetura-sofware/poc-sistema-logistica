87dcfef2c2091a3ae09ee84a1025a716257d3dad
PabloChristian <snakepablo@hotmail.com>
2021-09-30 12:06:44 -0300

    alteração url

--- frontend-portal-boa-entrega/src/environments/environment.ts
+++ frontend-portal-boa-entrega/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrlv1: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/',
-  imagensUrl: 'http://boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/'
+  apiUrlv1: 'http://api-boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/',
+  imagensUrl: 'http://api-boaentrega-cadastro.us-east-2.elasticbeanstalk.com/api/'
 };
 
 /*
